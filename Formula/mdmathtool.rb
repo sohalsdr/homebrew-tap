67ae1921db2187c3604c3520ecf7f489aa733ae6
Sohal Sudheer <62023554+sohalsdr@users.noreply.github.com>
2020-10-03 22:43:22 -0400

    Uploaded formula

---
+++ Formula/mdmathtool.rb
@@ -0,0 +1,22 @@
+class Javarepl < Formula
+    desc "A tool to help work with inline math in Markdown files, written in Java."
+    homepage "https://github.com/sohalsdr/mdMathTool"
+    # Remember to update the url for every release!
+    url "https://github.com/sohalsdr/mdMathTool/releases/download/1.1.2/mdMathTool_1.1.2.jar"
+    # Regen the SHA256 too, can be done using shasum -a 256 <file>
+    sha256 "5a87dfe8efa80b15c5f124f9f4c9343cc62d4df82d564010d5288449de85cceb"
+    license "MIT"
+    revision 1.1.2
+  
+    bottle :unneeded
+  
+    depends_on java: "1.8"
+  
+    def install
+      libexec.install "mdMathTool-#{version}.jar"
+      (libexec/"bin").write_jar_script libexec/"mdMathTool_#{version}.jar", "mdmathtool"
+      (libexec/"bin/mdmathtool").chmod 0755
+      (bin/"mdmathtool").write_env_script libexec/"bin/mdmathtool", Language::Java.java_home_env("1.8")
+    end
+  
+  end
---
+++ README.md
@@ -0,0 +1,13 @@
+# sohalsdr's Homebrew Tap
+
+## How do I install these formulae?
+`brew install sohalsdr/tap/<formula>`
+
+Or `brew tap sohalsdr/tap` and then `brew install <formula>`.
+
+## Available formulae
+
+mdMathTool
+
+## Documentation
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
